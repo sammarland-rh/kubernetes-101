6a98b8d660f2e6b44f78fa83cc8fa919d6c67c23
Sam Marland <smarland@redhat.com>
2021-02-11 08:59:55 +0000

    Cleaning up some issues in the repo

--- routes/badcode.js
+++ routes/badcode.js
@@ -3,22 +3,6 @@ var router = express.Router();
 
 sleepTime = process.env.SLEEP_TIME || 200
 
-router.get('/', function(req, res, next) {
-  msleep(sleepTime)
-  var i = 0;
-  var x = 0.0001;
-  while (i < 1000000) {
-    i++;
-    $x += sqrt($x);
-  }
-  res.render('index', { 
-    org: process.env.ORG, 
-    orgFull: process.env.ORG_FULL, 
-    eventName: process.env.EVENT_NAME,
-    bodyText: process.env.BODY_TEXT
-  });
-});
-
 router.get('/json', function(req, res, next) {
   var i = 0;
   var x = 0.0001;
